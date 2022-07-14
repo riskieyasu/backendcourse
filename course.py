ee485e5198f10988c7351550bc4b997daace9ad8
Riski Darmawan <Riski.Darmawan@medcoenergi.com>
2022-07-14 21:09:14 +0700

    naon weh

--- course.py
+++ course.py
@@ -1,16 +1,22 @@
 from asyncio import DatagramProtocol
 from unicodedata import category
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
+from flask import Flask, make_response
 from flask import request, jsonify
 import base64
+import jwt
 from itsdangerous import json
 from sqlalchemy import null
 from sqlalchemy.sql import func
 import datetime
-app = Flask(__name__)
 from sqlalchemy import delete
 from sqlalchemy import update
+from flask_cors import CORS,cross_origin
+from functools import wraps
+app = Flask(__name__)
+CORS(app, supports_credentials=True)
+
+# cors = CORS(app, resources={r"/*/": {"origins": "*"}}) 
 app.config['SECRET_KEY']='secret'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:Raikonen04@localhost:5432/course?sslmode=disable' #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://DB_USER:PASSWORD@HOST/DATABASE'
@@ -28,10 +34,11 @@ class Topic(db.Model):
 	name =db.Column(db.String(50), nullable=False)
 	course = db.relationship('Course', backref='ownera', lazy='dynamic')
 	
-class Course(db.Model):
+class Course(db.Model): 
 	id = db.Column(db.Integer, primary_key=True, index=True)
 	title =db.Column(db.String(50), nullable=False)
 	topic_id=db.Column(db.Integer, db.ForeignKey('topic.id'), nullable=False)
+	teacher_id=db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 	description =db.Column(db.String(50), nullable=True)
 	students = db.relationship('Coursedata', backref='ownere', lazy='dynamic')
 	
@@ -69,6 +76,43 @@ def auth(a):
 	elif a == 'passkey':
 		return c[1]
 
+@app.route('/login', methods=['POST'])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'], supports_credentials=True) 
+def login():
+	idd = auth('name')
+	idd_2= auth('passkey')
+	us = Users.query.filter_by(name = idd).first()
+	token = jwt.encode({
+                'user': idd,
+				'passkey' :idd_2,
+                'exp': datetime.datetime.now() + datetime.timedelta(hours=24)
+            },'secret' ,algorithm='HS256'
+            )
+	if not us or auth('passkey') != us.passkey:
+		return "wrong credentials", 400 
+		
+	resp = make_response("token generated")
+	resp.set_cookie('username', value=idd,expires=datetime.datetime.now() + datetime.timedelta(hours=24), path='/', samesite='Lax',)
+	resp.set_cookie('token',value=token,expires=datetime.datetime.now() + datetime.timedelta(hours=24), path='/',samesite='Lax',)
+	resp.set_cookie('role',value=us.role,expires=datetime.datetime.now() + datetime.timedelta(hours=24), path='/',samesite='Lax',)
+	return resp, 201
+		
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        if not token:
+            return 'Unauthorized Access!'
+        return f(*args, **kwargs)
+
+    return decorated
+
+@app.route('/tokenauth', methods=['POST'])
+@token_required
+def test():
+    return "Authorized"
 
 @app.route('/index')
 def index():
@@ -106,14 +150,14 @@ def create_course():
 	u = Course(
 			title=data['title'], 
             topic_id=data['topic_id'],
-			description = data['description']
+			description = data['description'],
+			teacher_id = us.id
 		)
 	db.session.add(u)
 	usa = Course.query.filter_by(title = data['title']).first()
 	for i in data['prerequisite']:
 		psa = Course.query.filter_by(title = i).first()
-		if 'prerequisite' in data:
-			u = Prerequisite(
+		u = Prerequisite(
 			course_id=usa.id, 
             preq_course_id=psa.id,
 			)
@@ -164,9 +208,9 @@ def enroll_course():
 			'message': 'all data not given'
 		}), 400    
 	
-	title = Course.query.filter_by(title = data['title']) .first()
+	titlee = Course.query.filter_by(title = data['title']).first()
 		
-	if not title:
+	if not titlee:
 		return jsonify({
 			'error': 'Bad Request',
 			'message': 'no course found'
@@ -184,8 +228,7 @@ def enroll_course():
 	preq = Prerequisite.query.filter_by(course_id = course.id).count()
 	pre = Prerequisite.query.filter_by(course_id = course.id).all()
 	us_cou = Coursedata.query.filter((Coursedata.user_id == us.id) & (Coursedata.status_id == 1)).count()
-	for i in range(len(pre)):
-		us_cou_1 = Coursedata.query.filter((Coursedata.user_id == us.id) & (Coursedata.course_id == pre[i].preq_course_id) & (Coursedata.status_id == 2)).all()
+	
 	
 	if a:
 		return jsonify({ 
@@ -197,9 +240,9 @@ def enroll_course():
 			'error': 'Bad Request',
 			'message': 'exceeded limitations'
 		}), 400 
-
-
-	if not us_cou_1 and preq != 0 :
+	for i in range(len(pre)):
+		us_cou_1 = Coursedata.query.filter((Coursedata.user_id == us.id) & (Coursedata.course_id == pre[i].preq_course_id) & (Coursedata.status_id == 2)).all()
+		if not us_cou_1 and preq != 0 :
 			return jsonify({ 
 			'error': 'Bad Request',
 			'message': 'not meet minimum requirements'
@@ -211,7 +254,9 @@ def enroll_course():
 	
 	db.session.add(coursedata)
 	db.session.commit()
-	return 'Enrolled', 201
+	return jsonify({ 
+			'message': 'Enrolled'
+		}), 201
 
 @app.route('/course/<id>')
 def get_courseid(id):
@@ -239,7 +284,7 @@ def get_courseid(id):
 def get_course():
 	idd = auth('name')
 	us = Users.query.filter_by(name = idd).first()
-	if not us or auth('passkey') != us.passkey or us.role != 'user':
+	if not us or auth('passkey') != us.passkey:
 		return jsonify({ 
 			'error': 'Bad Request',
 			'message': 'not authenthicated'
@@ -249,8 +294,10 @@ def get_course():
 
 	
 	return {
-		'1_student_name': courses.name, 
-		'2_courses': [{
+		'a_student_name': courses.name, 
+		'b_student_id': us.id, 
+		'c_role': us.role, 
+		'd_courses': [{
 				'course_id'    : cours.ownere.id,
 				'course_title' : cours.ownere.title,
 				'status' : cours.owneru.status,
@@ -258,15 +305,33 @@ def get_course():
 			]
 		}
 
-@app.route('/searchcourse')
-def get_coursesbytopic():
+@app.route('/teachercourse', methods=['GET'])
+def getteachercourse():
+	idd = auth('name')
+	us = Users.query.filter_by(name = idd).first()
+	course = Course.query.filter_by(teacher_id = us.id).all()
+	y =[]
+	for cours in course :
+		total_students = Coursedata.query.filter_by(course_id=cours.id).count()
+		y.append({
+			'course_id' : cours.id,
+			'course_name' : cours.title,
+			'total_students' : total_students
+			}) 
+	return jsonify(y)
+
+@app.route('/searchcourse', methods=['POST'])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'], supports_credentials=True) 
+def coursesbytopic():
 	data = request.get_json()
-	if not 'topic' in data and not 'name' in data and not 'description' in data:
+	if not 'topic' in data and not 'name' in data and not 'description' in data and not 'id' in data:
 		return jsonify({ 
 			'error': 'Bad Request',
 			'message': 'parameter not inserted'
 		}), 400 
-	
+	if 'id' in data:
+		course = Course.query.filter_by(id=data['id']).first()
+		return course.title
 	if 'topic' in data:
 		topic = Topic.query.filter(Topic.name.ilike('%' + data['topic'] + '%'))
 		a = jsonify([
@@ -275,19 +340,24 @@ def get_coursesbytopic():
 			'2_courses': [{
 					 '1_id' : member.id,
 		   			 '2_nama': member.title,
-					 '3_description' : member.description
+					 '3_description' : member.description,
+					 '4_topic_id':member.topic_id
 			} for member in cours.course 
 			]
+			
 			} for cours in topic
 	     ])
 		return a
 
 	if 'name' in data :
 		course = Course.query.filter(Course.title.ilike('%' + data['name'] + '%'))
+		
 		b = jsonify([
 		{
 			'1_name':cours.title, 
-			'2_description' : cours.description
+			'2_description' : cours.description,
+			'3_topic_id' : cours.topic_id,
+			'4_id':cours.id
 			} for cours in course
 		])
 		return b
@@ -301,16 +371,31 @@ def get_coursesbytopic():
 			} for cours in coursea
 		])
 		return c
+@app.route('/topicidsearch', methods=['POST'])
+def search_topic():
+	
+	data = request.get_json()
+	topic = Topic.query.filter(Topic.name.ilike('%' + data['topic'] + '%'))
+	
+	a= ([
+		{    
+			'Topic':id.id, 
+		}for id in topic
+	])
+	return str(a[0]['Topic'])
+	
+
 
 @app.route('/topiclist')
 def get_topic():
 	
 	topic = Topic.query.all()
+	
 	y =[]
 	for a in topic :
 		x = a.course.count()
 		z = a.name
-		y.append({'name':z, 'total_courses': x}) 
+		y.append({'name':z ,'total_courses':x}) 
 	return jsonify([
 		{    
 			'Topic':y[j], 
@@ -337,6 +422,20 @@ def get_prerequisite(id):
 		} for cours in course
 		 ])
 	return c
+@app.route('/getprerequisite/<id>')
+def get_cprerequisite(id):
+	print(id)
+	us_cou_1 = Prerequisite.query.filter_by(course_id = id).all()
+	a = []
+	for i in range(len(us_cou_1)):
+		pre = Course.query.filter_by(id=us_cou_1[i].preq_course_id).first()
+		a.append(pre)
+	c = jsonify([
+		{
+		'name':a[cours].title, 
+		} for cours in  range(len(a))
+		 ])
+	return c
 
 @app.route('/cancel/<id>', methods=['DELETE'])
 def pass_course(id):
@@ -356,6 +455,67 @@ def pass_course(id):
 	return {
 		'message': 'success'
 		}, 201
+
+@app.route('/deletecourse/<id>', methods=['DELETE'])
+
+def delete_course(id):
+	
+	print(id)
+	idd = auth('name')
+	us = Users.query.filter_by(name = idd).first()
+	if not us or auth('passkey') != us.passkey or us.role != 'teacher':
+		return jsonify({ 
+			'error': 'Bad Request',
+			'message': 'not authenthicated'
+		}), 400 
+		
+	coursedata = Course.query.filter((Course.id==id) & (Course.teacher_id==us.id)).first()
+	Prerequisite.query.filter((Prerequisite.course_id==id)).delete()
+	if not coursedata:
+		return jsonify({ 
+			'error': 'Bad Request',
+			'message': 'not authenthicated'
+		}), 400 
+	db.session.delete(coursedata)
+	db.session.commit()	
+	# Coursedata.query.filter_by(course_id=id).delete()
+	
+	
+	# db.session.commit()
+	return {
+		'message': 'success'
+		}, 201
+
+
+@app.route('/delet/<id>', methods=['DELETE'])
+
+def delete_coursedata(id):
+	
+	print(id)
+	idd = auth('name')
+	us = Users.query.filter_by(name = idd).first()
+	if not us or auth('passkey') != us.passkey or us.role != 'teacher':
+		return jsonify({ 
+			'error': 'Bad Request',
+			'message': 'not authenthicated'
+		}), 400 
+		
+	# coursedata = Course.query.filter((Course.id==id) & (Course.teacher_id==us.id)).first()
+	
+	# if not coursedata:
+	# 	return jsonify({ 
+	# 		'error': 'Bad Request',
+	# 		'message': 'not authenthicated'
+	# 	}), 400 
+	# db.session.delete(coursedata)
+	# db.session.commit()	
+	Coursedata.query.filter_by(course_id=id).delete()
+	db.session.commit()
+	return {
+		'message': 'success'
+		}, 201
+
+
 
 @app.route('/pass/<id>', methods=['PUT'])
 def pass_c(id):
@@ -383,25 +543,12 @@ def create_user():
 			'error': 'Bad Request',
 			'message': 'all data not given'
 		}), 400
-	u = Users(
-			name=data['name'],
-			role = data['role'],
-			passkey=data['passkey'],
-
-		)
-	db.session.add(u)
-	db.session.commit()
-	return {
-		'success': 'yes', 
-	}, 201
-@app.route('/daftar', methods=['POST'])
-def daftar():
-	data = request.get_json()
-	if not 'name' in data or not 'role' in data or not 'passkey'  in data:
-		return jsonify({
+	us = Users.query.filter_by(name = data['name']).first()
+	if us:
+		return jsonify({ 
 			'error': 'Bad Request',
-			'message': 'all data not given'
-		}), 400
+			'message': 'exist'
+		}), 400 
 	u = Users(
 			name=data['name'],
 			role = data['role'],
@@ -413,11 +560,13 @@ def daftar():
 	return {
 		'success': 'yes', 
 	}, 201
+
 
 @app.route('/user', methods=['PUT'])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'], supports_credentials=True) 
 def updateuser():
 	data = request.get_json()
-	if not 'name' in data or not 'role' in data or not 'passkey'  in data:
+	if not 'name' in data:
 		return jsonify({
 			'error': 'Bad Request',
 			'message': 'all data not given'
@@ -425,8 +574,6 @@ def updateuser():
 	idd = auth('name')
 	us = Users.query.filter_by(name = idd).first()
 	us.name = data['name']
-	us.role = data['role']
-	us.passkey = data['passkey']
 	db.session.commit()
 	return {
 		'success': 'yes', 
@@ -435,12 +582,13 @@ def updateuser():
 @app.route('/topcourse')
 def get_topcourse():
 	
-	a = db.engine.execute("select course.title, count(course.title) as frequency from coursedata join course on coursedata.course_id = course.id group by course.title order by frequency desc limit 5")
+	a = db.engine.execute("select course.title, count(course.title) as frequency from coursedata join course on coursedata.course_id = course.id group by course.title order by frequency desc limit 6")
 	b = []
 	c = 0
 	for i in a:
 		c = c +1
-		b.append({'1_Rank' : c,'2_course_name' : i[0],'3_students': i[1]})
+		tes = Course.query.filter_by(title = i[0]).first()
+		b.append({'a_Rank' : c,'b_course_name' : i[0],'c_students': i[1],'topic_id' : tes.topic_id, 'desc' : tes.description})
 	return jsonify(b)
 
 @app.route('/topstudent')
@@ -451,5 +599,5 @@ def get_topstudent():
 	c = 0
 	for i in a:
 		c = c +1
-		b.append({'1_Rank' : c,'2_student_name' : i[0],'3_courses_completed': i[1]})
+		b.append({'a_Rank' : c,'b_student_name' : i[0],'c_courses_completed': i[1]})
 	return jsonify(b)
